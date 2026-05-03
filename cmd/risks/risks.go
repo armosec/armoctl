932badd972556ca705538e1e70bc03d1786b6d94
Ben <ben@armosec.io>
2026-05-03 11:04:57 +0300

    risks: wire 'exceptions' subcommand group Signed-off-by: Ben <ben@armosec.io>

--- cmd/risks/risks.go
+++ cmd/risks/risks.go
@@ -11,5 +11,14 @@ func Cmd(clientFor cliclient.ClientFor) *cobra.Command {
 	c.AddCommand(ListCmd(clientFor))
 	c.AddCommand(ResourcesCmd(clientFor))
 	c.AddCommand(SeveritiesCmd(clientFor))
+
+	exc := &cobra.Command{Use: "exceptions", Short: "Manage security-risk exception policies (risk acceptance)"}
+	exc.AddCommand(ExceptionsListCmd(clientFor))
+	exc.AddCommand(ExceptionsGetCmd(clientFor))
+	exc.AddCommand(ExceptionsCreateCmd(clientFor))
+	exc.AddCommand(ExceptionsUpdateCmd(clientFor))
+	exc.AddCommand(ExceptionsDeleteCmd(clientFor))
+	c.AddCommand(exc)
+
 	return c
 }
